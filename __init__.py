3d69e4c46713928e8a81c2badba8d80d0b1fb085
srigomathikarthik17-cloud <srigomathikarthik17@gmail.com>
2026-04-09 23:34:30 +0530

    add this files under app/analyzer

--- __init__.py
+++ __init__.py
@@ -1,22 +1,4 @@
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate
+from .rule_based import detect_rule_based
+from .ml_model import LogAnomalyModel
 
-db = SQLAlchemy()
-migrate = Migrate()
-
-def create_app():
-    app = Flask(__name__)
-
-    # Load configuration
-    app.config.from_object("app.config.Config")
-
-    # Initialize extensions
-    db.init_app(app)
-    migrate.init_app(app, db)
-
-    # Register blueprints
-    from .routes import main
-    app.register_blueprint(main)
-
-    return app
+__all__ = ["detect_rule_based", "LogAnomalyModel"]
